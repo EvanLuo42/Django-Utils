c4b70c1007f08c6737a4a9f1b87c7b85ff379e54
Ziyun Luo <ziyun.luo@icloud.com>
2022-04-27 10:51:55 +0800

    feat: Rate limit middleware

---
+++ middleware.py
@@ -0,0 +1,11 @@
+class RateLimitMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        identify = request.META.get('REMOTE_ADDR')
+        requested_times = cache.get(identify)
+        if requested_times is not None:
+            if int(requested_times) >= settings.REQUEST_LIMIT:
+                return JsonResponse({'status': 'error', 'message': _('Too many requests')}, status=400)
+            else:
+                cache.set(identify, requested_times + 1)
+        else:
+            cache.set(identify, 1, settings.REQUEST_LIMIT_TIME)
