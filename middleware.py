17947f004e4119d9ec69323e8621f5f8c30654ef
Ziyun Luo <ziyun.luo@icloud.com>
2022-04-27 10:52:43 +0800

    update: middleware.py

--- middleware.py
+++ middleware.py
@@ -1,10 +1,15 @@
+from django.core.cache import cache
+from django.http import JsonResponse
+from django.utils.deprecation import MiddlewareMixin
+
+
 class RateLimitMiddleware(MiddlewareMixin):
     def process_request(self, request):
         identify = request.META.get('REMOTE_ADDR')
         requested_times = cache.get(identify)
         if requested_times is not None:
             if int(requested_times) >= settings.REQUEST_LIMIT:
-                return JsonResponse({'status': 'error', 'message': _('Too many requests')}, status=400)
+                return JsonResponse({'status': 'error', 'message': 'Too many requests'}, status=400)
             else:
                 cache.set(identify, requested_times + 1)
         else:
